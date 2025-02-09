e265ef056fb6256a277dce21c8d0e26804b7fed3
Paul Mayer <paul@paul-mayer.eu>
2025-02-10 00:30:11 +0100

    init movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,25 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
- 
+const ROTATION = 0.1
+
+func _physics_process(delta: float) -> void:
+	
+	
+	#velocity = BOOSTER_POWER.normalized() + BOOSTER_POWER + 
+	var booster_fire := Input.get_axis("ui_left", "ui_right")
+	
+	print(rotation_degrees)
+
+	
+	if (booster_fire == -1):
+		rotate(-ROTATION)
+		
+	if (booster_fire == 1):
+		rotate(ROTATION)
+	
+	
+	print(velocity)
+	velocity = Vector2(0,-10).rotated(rotation_degrees)
+
+	move_and_slide()
