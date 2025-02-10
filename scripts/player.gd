267afb9b55e30f2cb442625ec4c309d1b63abdb7
Paul Mayer <paul@paul-mayer.eu>
2025-02-10 22:21:24 +0100

    feat: better movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,17 +9,11 @@ func _physics_process(delta: float) -> void:
 	#velocity = BOOSTER_POWER.normalized() + BOOSTER_POWER + 
 	var booster_fire := Input.get_axis("ui_left", "ui_right")
 	
-	
-	print(rotation)
-	
 	if (booster_fire == -1):
 		rotate(-ROTATION)
 		
 	if (booster_fire == 1):
 		rotate(ROTATION)
 	
-	
-	print(velocity)
 	velocity = Vector2(0,-SPEED).rotated(rotation)
-
 	move_and_slide()
