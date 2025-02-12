063be4809f8938711a575ac4b8d1b10fbe70b57a
Paul Mayer <paul@paul-mayer.eu>
2025-02-12 23:08:00 +0100

    feat: teleport done

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-const SPEED = 400;
+const SPEED = 800;
+@onready var screen_size = get_viewport_rect().size
+
+
+func _process(delta: float) -> void:
+	screen_size = get_viewport_rect().size
+	position.x = wrapf(position.x, -screen_size.x/2,screen_size.x/2 )
+	position.y = wrapf(position.y, -screen_size.y/2, screen_size.y/2)
+	position += transform.x * SPEED * delta;
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position += transform.x * SPEED * delta;
+	await get_tree().create_timer(1.0).timeout  # Wait for 3 seconds
+	queue_free()  
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
+const SPEED = 400.0
 const ROTATION = 0.1
 const BULLET = preload("res://scenes/bullet.tscn")
 
-func _process(delta: float) -> void:
+@onready var screen_size = get_viewport_rect().size
+
+func _ready():
+	pass 
 	
+func _process(delta: float) -> void:
+	screen_size = get_viewport_rect().size
+
+	position.x = wrapf(position.x, -screen_size.x/2,screen_size.x/2 )
+	position.y = wrapf(position.y, -screen_size.y/2, screen_size.y/2)
+		
 	if Input.is_action_just_pressed("ui_down"):
 		var bullet_instance = BULLET.instantiate()
-	
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.rotation_degrees = rotation_degrees - 90
 		bullet_instance.position = position
 	
+
+		
 func _physics_process(delta: float) -> void:
-	
-	
-	#velocity = BOOSTER_POWER.normalized() + BOOSTER_POWER + 
+
 	var booster_fire := Input.get_axis("ui_left", "ui_right")
 	
 	if (booster_fire == -1):
